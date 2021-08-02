2bd4ab5e5530df715ff6c02a4f7f7e15f3197552
Nate Bliton <natebliton@gmail.com>
2021-08-01 23:42:10 -0400

    added tom1 and tom2 v1

--- proto.cpp
+++ proto.cpp
@@ -1,3 +1,5 @@
+// working copy
+
 #include "daisysp.h"
 #include "daisy_seed.h"
 
@@ -21,12 +23,14 @@ static Metro clock;
 // drum oscillators
 Oscillator osc;
 WhiteNoise noise;
+Oscillator tomOsc1, tomOsc2;
 
 // for sine testing
 static Oscillator osc_sine;
 static Oscillator osc_saw;
 
 AdEnv kickVolEnv, kickPitchEnv, snareEnv;
+AdEnv tom1Env, tom2Env;
 
 // array to keep track of whether pegs are in each hole
 bool pegs[64];
@@ -52,6 +56,9 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
 
     // drum variables
     float osc_out, noise_out, snr_env_out, kck_env_out, sig;
+
+    // tom variables
+    float tom1_out, tom2_out, tom1_env_out, tom2_env_out;
 
     // prepare the audio block
     for(size_t i = 0; i < size; i += 2)
@@ -80,16 +87,26 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
             if(pegs[metroStep + 16]){
                 snareEnv.Trigger();
             }
+
+            if(pegs[metroStep + 32]){
+                tom1Env.Trigger();
+            }
+
+            if(pegs[metroStep + 48]) {
+                tom2Env.Trigger();
+            }
         }
 
         // update test oscillator, get value for current sample
         sine = osc_sine.Process();
         saw = osc_saw.Process();
-
+        
         // process drums
         snr_env_out = snareEnv.Process();
         kck_env_out = kickVolEnv.Process();
-
+        tom1_env_out = tom1Env.Process();
+        tom2_env_out = tom2Env.Process();
+        
         // apply the pitch envelope to the kick
         osc.SetFreq(kickPitchEnv.Process());
         // set the kick volume to the envelope's output
@@ -102,8 +119,14 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
         // set the sample to the correct volume
         noise_out *= snr_env_out;
 
-        // mix the two signals at half volume
-        sig = 0.5 * noise_out + 0.5 * osc_out;
+        // process tom1 and tom2
+        tomOsc1.SetAmp(tom1_env_out);
+        tomOsc2.SetAmp(tom2_env_out);
+        tom1_out = tomOsc1.Process();
+        tom2_out = tomOsc2.Process();
+
+        // mix the drum signals at half volume each
+        sig = (0.4 * noise_out) + (0.4 * osc_out) + (tom1_out * 0.4) + (tom2_out * 0.4);
 
         // left out
         out[i] = sig;
@@ -234,6 +257,18 @@ int main(void)
     osc.SetWaveform(Oscillator::WAVE_TRI);
     osc.SetAmp(1);
 
+    // initialize osc for tom1
+    tomOsc1.Init(sample_rate);
+    tomOsc1.SetWaveform(Oscillator::WAVE_SIN);
+    tomOsc1.SetFreq(400);
+    tomOsc1.SetAmp(1);
+
+    // initialize osc for tom2
+    tomOsc2.Init(sample_rate);
+    tomOsc2.SetWaveform(Oscillator::WAVE_SIN);
+    tomOsc2.SetFreq(300);
+    tomOsc2.SetAmp(1);
+
     // initialize noise
     noise.Init();
 
@@ -253,11 +288,25 @@ int main(void)
 
     // kick volume envelope
     kickVolEnv.Init(sample_rate);
-    kickVolEnv.SetTime(ADENV_SEG_ATTACK, 0.01);
+    kickVolEnv.SetTime(ADENV_SEG_ATTACK, 0.001);
     kickVolEnv.SetTime(ADENV_SEG_DECAY, 1);
     kickVolEnv.SetMax(1);
     kickVolEnv.SetMin(0);
 
+    // tom1 volume envelope
+    tom1Env.Init(sample_rate);
+    tom1Env.SetTime(ADENV_SEG_ATTACK, 0.001);
+    tom1Env.SetTime(ADENV_SEG_DECAY, 0.25);
+    tom1Env.SetMax(1);
+    tom1Env.SetMin(0);
+    
+    // tom2 volume envelope
+    tom2Env.Init(sample_rate);
+    tom2Env.SetTime(ADENV_SEG_ATTACK, 0.001);
+    tom2Env.SetTime(ADENV_SEG_DECAY, 0.25);
+    tom2Env.SetMax(1);
+    tom2Env.SetMin(0);
+    
 
     // start calling the callback function
     hardware.StartAudio(AudioCallback);

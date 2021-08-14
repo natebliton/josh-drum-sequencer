7e6ebfb48b91e958ad7c6aefce987aeb1c237081
Nate Bliton <natebliton@gmail.com>
2021-08-14 13:59:27 -0400

    renamed shiftPins variable in ShiftOut

--- proto.cpp
+++ proto.cpp
@@ -14,7 +14,6 @@ using namespace daisysp;
 #define GPIO_5    {  DSY_GPIOC, 8 }
 #define GPIO_6    {  DSY_GPIOD, 2 }
 
-
 // Declare a DaisySeed object called hardware
 DaisySeed hardware;
 
@@ -145,50 +144,50 @@ class ShiftOut
     ~ShiftOut() {}
 
         /*
-    ledPins[0] GPIO_4 = 74HC595 SCK pin 11 (clock)
-    ledPins[1] GPIO_5 = 74HC595 EN pin 12 (chip enable / latch ) 
-    ledPins[2] GPIO_6 = 74HC595 SER pin 14 (data) 
+    shiftPins[0] GPIO_4 = 74HC595 SCK pin 11 (clock)
+    shiftPins[1] GPIO_5 = 74HC595 EN pin 12 (chip enable / latch ) 
+    shiftPins[2] GPIO_6 = 74HC595 SER pin 14 (data) 
     */
 
     void Init(){
         //setup the pins as outputs
         for(size_t i = 0; i < 3; i++) {
-            ledPins[i].pin  = pins[i];
-            ledPins[i].mode = DSY_GPIO_MODE_OUTPUT_PP;
-            ledPins[i].pull = DSY_GPIO_NOPULL;
-            dsy_gpio_init(&ledPins[i]);
-            dsy_gpio_write(&ledPins[i], 1);
+            shiftPins[i].pin  = pins[i];
+            shiftPins[i].mode = DSY_GPIO_MODE_OUTPUT_PP;
+            shiftPins[i].pull = DSY_GPIO_NOPULL;
+            dsy_gpio_init(&shiftPins[i]);
+            dsy_gpio_write(&shiftPins[i], 1);
         }
     }
 
     void Write(uint8_t data) {
         //enable serial buffer , disable write to output buffer
-        dsy_gpio_write(&ledPins[1], 0);
+        dsy_gpio_write(&shiftPins[1], 0);
         for (uint8_t i = 0; i < 8; ++i) {
             //check the least significant bit
             //and write the data pin high or low
             if ( (data & BITMASK) == BITMASK) {
-                dsy_gpio_write(&ledPins[2], 1);  
+                dsy_gpio_write(&shiftPins[2], 1);  
             } 
             else {
-                dsy_gpio_write(&ledPins[2], 0);   
+                dsy_gpio_write(&shiftPins[2], 0);   
             }
             //shift data down one bit, i.e. parallel to serial 
             data >>= 1;
             //clock low   
-            dsy_gpio_write(&ledPins[0], 0);
+            dsy_gpio_write(&shiftPins[0], 0);
             System::Delay(1);
             //dsy_system_delay(1);
             //clock high
-            dsy_gpio_write(&ledPins[0], 1);
+            dsy_gpio_write(&shiftPins[0], 1);
             System::Delay(1);
             //dsy_system_delay(1);
         }
         // after all 8 bits,  latch data from 74HC595 serial buffer to its output with a pulse
-        dsy_gpio_write(&ledPins[1], 1);
+        dsy_gpio_write(&shiftPins[1], 1);
         System::Delay(1);
         //dsy_system_delay(1);
-        dsy_gpio_write(&ledPins[1], 0);
+        dsy_gpio_write(&shiftPins[1], 0);
     }  
 
   private:
@@ -196,7 +195,7 @@ class ShiftOut
     dsy_gpio_pin pins[3] = {
         GPIO_4,  GPIO_5,  GPIO_6
     };
-    dsy_gpio ledPins[3];
+    dsy_gpio shiftPins[3];
 
     const uint8_t BITMASK = 0x1;
 

64c91b8de4991dae982ad3cb96485db94c6c8774
Nate Bliton <natebliton@gmail.com>
2021-10-03 22:34:39 -0400

    replaced homebrew drums with Daisy objects

--- proto.cpp
+++ proto.cpp
@@ -32,17 +32,15 @@ DaisySeed hardware;
 
 static Metro clock;
 
-// drum oscillators
-Oscillator osc;
-WhiteNoise noise;
-Oscillator tomOsc1, tomOsc2;
+// drum objects
+AnalogBassDrum kickDrum;
+AnalogBassDrum kickHigh;
+AnalogSnareDrum snareDrum;
+HiHat<> hiHat;
 
 // for sine testing
 static Oscillator osc_sine;
 static Oscillator osc_saw;
-
-AdEnv kickVolEnv, kickPitchEnv, snareEnv;
-AdEnv tom1Env, tom2Env;
 
 // array to keep track of whether pegs are in each hole
 static bool pegs[64];
@@ -67,10 +65,7 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
     uint8_t tic;
 
     // drum variables
-    float osc_out, noise_out, snr_env_out, kck_env_out, sig;
-
-    // tom variables
-    float tom1_out, tom2_out, tom1_env_out, tom2_env_out;
+    float kick_out, snare_out, kickHigh_out, hiHat_out, sig;
 
     // prepare the audio block
     for(size_t i = 0; i < size; i += 2)
@@ -87,69 +82,41 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
             }
 
             if(pegs[metroStep]){
-                kickVolEnv.Trigger();
-                kickPitchEnv.Trigger();
-            
-                freq = rand() %500;
-                osc_sine.SetFreq(freq);
-                osc_saw.SetFreq(freq);
-
+                kickDrum.Trig();
             //    pegLEDs.flash(metroStep);
 
             }
 
             if(pegs[metroStep + 16]){
-                snareEnv.Trigger();
+                snareDrum.Trig();
              //   pegLEDs.flash(metroStep + 16);
             }
 
             if(pegs[metroStep + 32]){
-                tom1Env.Trigger();
+                kickHigh.Trig();
              //   pegLEDs.flash(metroStep + 32);
             }
 
             if(pegs[metroStep + 48]) {
              //   pegLEDs.flash(metroStep + 48);
-                tom2Env.Trigger();
+                hiHat.Trig();
             }
         }
 
-        // update test oscillator, get value for current sample
-        sine = osc_sine.Process();
-        saw = osc_saw.Process();
-        
         // process drums
-        snr_env_out = snareEnv.Process();
-        kck_env_out = kickVolEnv.Process();
-        tom1_env_out = tom1Env.Process();
-        tom2_env_out = tom2Env.Process();
-        
-        // apply the pitch envelope to the kick
-        osc.SetFreq(kickPitchEnv.Process());
-        // set the kick volume to the envelope's output
-        osc.SetAmp(kck_env_out);
-        // process the next oscillator sample
-        osc_out = osc.Process();
-
-        // get the next snare sample
-        noise_out = noise.Process();
-        // set the sample to the correct volume
-        noise_out *= snr_env_out;
-
-        // process tom1 and tom2
-        tomOsc1.SetAmp(tom1_env_out);
-        tomOsc2.SetAmp(tom2_env_out);
-        tom1_out = tomOsc1.Process();
-        tom2_out = tomOsc2.Process();
-
+        kick_out = kickDrum.Process();
+        snare_out = snareDrum.Process();
+        kickHigh_out = kickHigh.Process();
+        hiHat_out = hiHat.Process();
+  
         // mix the drum signals at half volume each
-        sig = (0.4 * noise_out) + (0.4 * osc_out) + (tom1_out * 0.4) + (tom2_out * 0.4);
+        sig = (0.4 * kick_out) + (0.4 * snare_out) + (kickHigh_out * 0.4) + (hiHat_out * 0.4);
 
         // left out
         out[i] = sig;
 
         // right out
-        out[i + 1] = saw;
+        out[i + 1] = sig;
     }
 }
 
@@ -385,74 +352,12 @@ int main(void)
 
     // initialize Metro object at 2 hz
     clock.Init(8, sample_rate);
-
-    // set parameters for sine oscillator object
-    osc_sine.Init(sample_rate);
-    osc_sine.SetWaveform(Oscillator::WAVE_SIN);
-    osc_sine.SetFreq(100);
-    osc_sine.SetAmp(0.25);
-
-    // and also for saw oscillator
-    osc_saw.Init(sample_rate);
-    osc_saw.SetWaveform(Oscillator::WAVE_SAW);
-    osc_saw.SetFreq(100);
-    osc_saw.SetAmp(0.25);
-
-    // initialize oscillator for kickdrum
-    osc.Init(sample_rate);
-    osc.SetWaveform(Oscillator::WAVE_TRI);
-    osc.SetAmp(1);
-
-    // initialize osc for tom1
-    tomOsc1.Init(sample_rate);
-    tomOsc1.SetWaveform(Oscillator::WAVE_SIN);
-    tomOsc1.SetFreq(400);
-    tomOsc1.SetAmp(1);
-
-    // initialize osc for tom2
-    tomOsc2.Init(sample_rate);
-    tomOsc2.SetWaveform(Oscillator::WAVE_SIN);
-    tomOsc2.SetFreq(300);
-    tomOsc2.SetAmp(1);
-
-    // initialize noise
-    noise.Init();
-
-    // initialize envelopes, for snare
-    snareEnv.Init(sample_rate);
-    snareEnv.SetTime(ADENV_SEG_ATTACK, 0.01);
-    snareEnv.SetTime(ADENV_SEG_DECAY, 0.2);
-    snareEnv.SetMax(1);
-    snareEnv.SetMin(0);
-
-    // kick osc pitch (faster decay)
-    kickPitchEnv.Init(sample_rate);
-    kickPitchEnv.SetTime(ADSR_SEG_ATTACK, 0.01);
-    kickPitchEnv.SetTime(ADSR_SEG_DECAY, 0.05);
-    kickPitchEnv.SetMax(400);
-    kickPitchEnv.SetMin(50);
-
-    // kick volume envelope
-    kickVolEnv.Init(sample_rate);
-    kickVolEnv.SetTime(ADENV_SEG_ATTACK, 0.001);
-    kickVolEnv.SetTime(ADENV_SEG_DECAY, 1);
-    kickVolEnv.SetMax(1);
-    kickVolEnv.SetMin(0);
-
-    // tom1 volume envelope
-    tom1Env.Init(sample_rate);
-    tom1Env.SetTime(ADENV_SEG_ATTACK, 0.001);
-    tom1Env.SetTime(ADENV_SEG_DECAY, 0.25);
-    tom1Env.SetMax(1);
-    tom1Env.SetMin(0);
     
-    // tom2 volume envelope
-    tom2Env.Init(sample_rate);
-    tom2Env.SetTime(ADENV_SEG_ATTACK, 0.001);
-    tom2Env.SetTime(ADENV_SEG_DECAY, 0.25);
-    tom2Env.SetMax(1);
-    tom2Env.SetMin(0);
-    
+    // initialize drums
+    kickDrum.Init(sample_rate);
+    kickHigh.Init(sample_rate);
+    snareDrum.Init(sample_rate);
+    hiHat.Init(sample_rate);
 
     // start calling the callback function
     hardware.StartAudio(AudioCallback);
@@ -463,18 +368,6 @@ int main(void)
     static ShiftOut shiftOut;
     shiftOut.Init();
 
-/*
-    // initialize shift register input pins
-    dsy_gpio_pin pinsIn[8];
-    static dsy_gpio shiftInPins[8];
-    for(int i = 0; i < 8; i++){
-        pinsIn[i].pin = shiftGPIOinPins[i];
-        shiftInPins[i].pin = pinsIn[i];
-        shiftInPins[i].mode = DSY_GPIO_MODE_INPUT;
-        shiftInPins[i].pull = DSY_GPIO_PULLUP;
-        dsy_gpio_init(&shiftInPins[i]);
-    }
-    */
     static ShiftIn shiftIn;
     shiftIn.Init();
 

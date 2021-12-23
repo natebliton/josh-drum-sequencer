abe7e781aead928889bdc7d21f381eab04fe626b
Nate Bliton <natebliton@gmail.com>
2021-12-22 20:54:56 -0500

    added adc tempo knob and backstepped drums back to just kick and high kick, until more debugging

--- PegSequencer/PegSequencer.cpp
+++ PegSequencer/PegSequencer.cpp
@@ -38,6 +38,9 @@ static Metro clock;
 // int to keep track fo which step we are on (0-15)
 int metroStep = 0;
 float metroFreq = 32.0f;
+// variable to keep track of ADC knob
+float tempoKnob = 0.0f;
+int tempoKnobPin = 16;
 
 // array to keep track of whether pegs are in each hole
 static bool pegs[64];
@@ -83,12 +86,12 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in, AudioHandle::Interle
             kickHigh.Trig();
         }
 
-        if(testBeatArray[metroStep + 32] ) {
-            snareDrum.Trig();
+        if(pegs[metroStep + 32] ) {
+          //  snareDrum.Trig();
         }
-        if(testBeatArray[metroStep + 48])
+        if(pegs[metroStep + 48])
         {
-            hihat.Trig();
+           // hihat.Trig();
         }
 
 	}
@@ -97,14 +100,14 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in, AudioHandle::Interle
 	for(size_t i = 0; i < size; i += 2) {
 		kick_out = kickDrum.Process();
         kickHigh_out = kickHigh.Process();
-		snare_out = snareDrum.Process();
-        hihat_out = hihat.Process();
+	//	snare_out = snareDrum.Process();
+     //   hihat_out = hihat.Process();
 
         //osc_out = osc.Process();
 		//sigL = osc_out;
 
         // mono for now
-		sigL = sigR = (kick_out + kickHigh_out + snare_out + hihat_out) * 50;
+		sigL = sigR = (1.5f * kick_out + 1.5f * kickHigh_out + 0.5f*snare_out + hihat_out) * 10;
 		out[i] = sigL;
 		out[i + 1] = sigR;
 	}
@@ -217,11 +220,12 @@ int main(void)
 	// initialize drums
 	kickDrum.Init(sample_rate);
     kickHigh.Init(sample_rate);
-    kickHigh.SetFreq(200.0f);
-    snareDrum.Init(sample_rate);
+    kickHigh.SetFreq(100.0f);
+    kickHigh.SetDecay(0.2f);
+ //   snareDrum.Init(sample_rate);
 //    snareDrum.SetSustain()
-    hihat.Init(sample_rate);
-    hihat.SetDecay(0.01f);
+ //   hihat.Init(sample_rate);
+  //  hihat.SetDecay(0.01f);
 	//kickDrum.SetFreq(100.0f);
 	//kickDrum.SetDecay(0.5f);
 	
@@ -238,6 +242,15 @@ int main(void)
 
     inputPinsInit();
     
+    // set up adc
+    AdcChannelConfig adcConfig;
+    adcConfig.InitSingle(hw.GetPin(tempoKnobPin));
+
+    // initialize the adc with that config
+    hw.adc.Init(&adcConfig, 1);
+    // start reading values
+    hw.adc.Start();
+
     //  initialize array to be test beat
     for (int i = 0; i < 64; i++) {
         if(testBeatArray[i] == 1){
@@ -251,14 +264,18 @@ int main(void)
     // Loop forever
     for(;;)
     {
-        // Set the onboard LED
+        tempoKnob = hw.adc.GetFloat(0);
+        metroFreq = 10 + tempoKnob * 100;
+        clock.SetFreq(metroFreq);
+        
+                // Set the onboard LED
         hw.SetLed(led_state);
 
         // Toggle the LED state for the next time around.
         led_state = !led_state;
 
         // Wait 500ms
-        System::Delay(1000);
+        System::Delay(10);
 		//kickDrum.Trig();
 
         shiftPosition = 0;
